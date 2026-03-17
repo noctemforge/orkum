668f3556e4758f3f99067374fc673a895c8d7274
Tamish Dahiya <tamish.dahiya@gmail.com>
2026-03-18 02:44:52 +0530

    Refactor to optimise non-active file data

--- src/fs_util.rs
+++ src/fs_util.rs
@@ -1,44 +1,42 @@
-use slint::{Model, ModelNotify, ModelRc};
-use std::cell::RefCell;
+use slint::ModelNotify;
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::{Read, Seek, SeekFrom};
 use std::path::PathBuf;
 use std::rc::Rc;
+use std::{cell::RefCell, fs};
 
-use crate::{AppState, ByteData, FileEntry, HexRow};
+use crate::{AppState, HexModel};
 
-pub fn open_new_file(state: &AppState) {
-    let win = state.main_window.unwrap();
-    let files_model = state.open_files.clone();
+pub fn open_new_file(state: &mut AppState) {
     if let Some(path) = show_file_dialog() {
-        match File::open(&path) {
-            Ok(f) => {
-                let meta = f.metadata().unwrap();
-
-                let name = path
-                    .file_name()
-                    .unwrap_or_default()
-                    .to_string_lossy()
-                    .to_string();
-
-                let hex_model_handle = HexModel {
-                    file: f,
-                    file_size: meta.len(),
-                    pending_changes: RefCell::new(HashMap::new()),
-                    notify: ModelNotify::default(),
-                };
-
-                files_model.push(FileEntry {
-                    name: name.into(),
-                    content: ModelRc::new(hex_model_handle),
-                });
-
-                // Switch to the newly opened tab
-                win.set_active_tab((files_model.row_count() - 1) as i32);
-            }
-            Err(e) => eprintln!("Error reading file: {}", e),
+        if let Ok(meta) = fs::metadata(&path) {
+            let new_file_handle = Rc::new(HexModel {
+                path,
+                file_size: meta.len(),
+                pending_changes: RefCell::new(HashMap::new()),
+                notify: ModelNotify::default(),
+            });
+            state.open_files.push(new_file_handle);
+            state.active_file = Some(state.open_files.len() - 1);
         }
+
+        // match File::open(&path) {
+        //     Ok(f) => {
+        //         let name = path
+        //             .file_name()
+        //             .unwrap_or_default()
+        //             .to_string_lossy()
+        //             .to_string();
+
+        //         files_model.push(FileEntry {
+        //             name: name.into(),
+        //             content: ModelRc::new(hex_model_handle),
+        //         });
+
+        //         // Switch to the newly opened tab
+        //         win.set_active_tab((files_model.row_count() - 1) as i32);
+        //     }
+        //     Err(e) => eprintln!("Error reading file: {}", e),
+        // }
     }
 }
 
@@ -47,91 +45,6 @@ fn show_file_dialog() -> Option<PathBuf> {
         .set_title("Select a file")
         .set_directory(PathBuf::from("."))
         .pick_file()
-}
-
-struct HexModel {
-    file: std::fs::File,
-    file_size: u64,
-    pending_changes: RefCell<HashMap<u64, u8>>,
-    notify: ModelNotify,
-}
-
-impl Model for HexModel {
-    type Data = HexRow;
-
-    fn row_count(&self) -> usize {
-        (self.file_size as f64 / 16.0).ceil() as usize
-    }
-
-    fn row_data(&self, row: usize) -> Option<Self::Data> {
-        let mut buffer = [0u8; 16];
-        let pos = (row * 16) as u64;
-        let offset_str = format!("{:08X}", pos);
-
-        // Seek to the specific part of the large file
-        let mut file = &self.file;
-        file.seek(SeekFrom::Start(pos)).ok()?;
-        let n = file.read(&mut buffer).ok()?;
-        if n == 0 {
-            return None;
-        }
-
-        // let hex: Vec<ByteData> = buffer
-        //     .iter()
-        //     .enumerate()
-        //     .map(|(i, &b)| {
-        //         ByteData {
-        //             value: format!("{:02X}", b).into(),
-        //             is_modified: false, // You can check a HashMap here for pending changes
-        //         }
-        //     })
-        //     .collect();
-
-        let changes = self.pending_changes.borrow();
-
-        let hex: Vec<ByteData> = (0..16)
-            .map(|i| {
-                let abs_offset = pos + i as u64;
-                if let Some(&m_byte) = changes.get(&abs_offset) {
-                    ByteData {
-                        value: format!("{:02X}", m_byte).into(),
-                        is_modified: true,
-                    }
-                } else if i < n {
-                    ByteData {
-                        value: format!("{:02X}", buffer[i]).into(),
-                        is_modified: false,
-                    }
-                } else {
-                    ByteData {
-                        value: "  ".into(),
-                        is_modified: false,
-                    }
-                }
-            })
-            .collect();
-
-        let ascii_str = buffer[..n]
-            .iter()
-            .map(|&b| {
-                if b.is_ascii_graphic() || b == b' ' {
-                    b as char
-                } else {
-                    '.'
-                }
-            })
-            .collect::<String>();
-
-        Some(HexRow {
-            offset: offset_str.into(),
-            hex: Rc::new(slint::VecModel::from(hex)).into(),
-            ascii: ascii_str.into(),
-        })
-    }
-
-    fn model_tracker(&self) -> &dyn slint::ModelTracker {
-        &self.notify
-    }
 }
 
 // fn list_dir(path: PathBuf) -> io::Result<ModelRc<SharedString>> {
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,165 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+slint::include_modules!();
+
 mod fs_util;
 
-use slint::{ComponentHandle, PlatformError, VecModel, Weak};
-use std::rc::Rc;
+use slint::{ComponentHandle, Model, ModelNotify, ModelRc, PlatformError, VecModel, Weak};
+use std::io::{Read, Seek};
+use std::{cell::RefCell, collections::HashMap, fs::File, io::SeekFrom, path::PathBuf, rc::Rc};
 
-slint::include_modules!();
+struct HexModel {
+    path: PathBuf,
+    file_size: u64,
+    pending_changes: RefCell<HashMap<u64, u8>>,
+    notify: ModelNotify,
+}
+
+impl Model for HexModel {
+    type Data = RowData;
+
+    fn row_count(&self) -> usize {
+        (self.file_size as f64 / 16.0).ceil() as usize
+    }
+
+    fn row_data(&self, row: usize) -> Option<Self::Data> {
+        let pos = (row * 16) as u64;
+        let mut buffer = [0u8; 16];
+
+        let mut file = File::open(&self.path).ok()?;
+        file.seek(SeekFrom::Start(pos)).ok()?;
+        let n = file.read(&mut buffer).ok()?;
+        if n == 0 && self.file_size > 0 {
+            return None;
+        }
+
+        let changes = self.pending_changes.borrow();
+
+        let bytes: Vec<ByteData> = (0..16)
+            .map(|i| {
+                let abs_offset = pos + i as u64;
+                if let Some(&m_byte) = changes.get(&abs_offset) {
+                    ByteData {
+                        value: format!("{:02X}", m_byte).into(),
+                        is_modified: true,
+                    }
+                } else if i < n {
+                    ByteData {
+                        value: format!("{:02X}", buffer[i]).into(),
+                        is_modified: false,
+                    }
+                } else {
+                    ByteData {
+                        value: "  ".into(),
+                        is_modified: false,
+                    }
+                }
+            })
+            .collect();
+
+        let ascii: String = (0..n)
+            .map(|i| {
+                let b = changes.get(&(pos + i as u64)).cloned().unwrap_or(buffer[i]);
+                if b.is_ascii_graphic() || b == b' ' {
+                    b as char
+                } else {
+                    '.'
+                }
+            })
+            .collect();
+
+        Some(RowData {
+            offset: format!("{:08X}", pos).into(),
+            bytes: Rc::new(VecModel::from(bytes)).into(),
+            ascii: ascii.into(),
+        })
+    }
+
+    fn model_tracker(&self) -> &dyn slint::ModelTracker {
+        &self.notify
+    }
+}
 
 /// Top level app data references
 #[derive(Clone)]
 struct AppState {
-    open_files: Rc<VecModel<FileEntry>>,
-    main_window: Weak<AppWindow>,
+    // main_window: Weak<AppWindow>,
+    active_file: Option<usize>,
+    open_files: Vec<Rc<HexModel>>,
 }
 
 fn main() -> Result<(), PlatformError> {
-    let window = AppWindow::new()?;
+    let ui = AppWindow::new()?;
 
-    let files_model = Rc::new(VecModel::<FileEntry>::default());
-    window.set_files(files_model.clone().into());
+    let state = Rc::new(RefCell::new(AppState {
+        // main_window: ui.as_weak(),
+        open_files: Vec::new(),
+        active_file: None,
+    }));
 
-    let state = AppState {
-        main_window: window.as_weak(),
-        open_files: files_model.clone(),
+    let sync_ui = {
+        let ui_weak = ui.as_weak();
+        let state_rc = state.clone();
+        move || {
+            let ui = ui_weak.unwrap();
+            let st = state_rc.borrow();
+
+            // 1. Update Tabs
+            let tabs: Vec<FileEntry> = st
+                .open_files
+                .iter()
+                .enumerate()
+                .map(|(_, f)| {
+                    let has_changes = !f.pending_changes.borrow().is_empty();
+                    FileEntry {
+                        name: f
+                            .path
+                            .file_name()
+                            .unwrap_or_default()
+                            .to_string_lossy()
+                            .to_string()
+                            .into(),
+                        modified: has_changes,
+                    }
+                })
+                .collect();
+            ui.set_open_files(Rc::new(VecModel::from(tabs)).into());
+
+            // 2. Update Active File View
+            if let Some(idx) = st.active_file {
+                if let Some(active_file) = st.open_files.get(idx) {
+                    ui.set_hex_rows(ModelRc::new(active_file.clone()));
+                    ui.set_active_tab(idx as i32);
+                }
+            } else {
+                ui.set_active_tab(-1);
+            }
+        }
     };
 
-    window.on_open_file_clicked(move || fs_util::open_new_file(&state));
-    window.on_close_file_clicked(move |index| {
-        files_model.remove(index as usize);
+    ui.on_open_file_clicked(move || {
+        fs_util::open_new_file(&mut state.borrow_mut());
+        sync_ui();
     });
+    // ui.on_close_file_clicked(move |index| {
+    //     files_model.remove(index as usize);
+    // });
+    // window.on_byte_edited(move |f, r, c, val| {
+    //     if (f < files_model.row_count()) {
+    //         window.invoke_error_notification(SharedString::from("unloaded file edited"));
+    //         return;
+    //     }
+    //     let m_edit = files_model.row_data(f).unwrap().content;
+    //     // files_model[0];
+    //     if val.len() == 2 {
+    //         if let Ok(byte) = u8::from_str_radix(&val, 16) {
+    //             let abs_offset = (r as u64 * 16) + c as u64;
+    //             m_edit.pending_changes.borrow_mut().insert(abs_offset, byte);
+    //             m_edit.notify.row_changed(r as usize);
+    //         }
+    //     }
+    // });
 
-    window.run()
+    ui.run()
 }
